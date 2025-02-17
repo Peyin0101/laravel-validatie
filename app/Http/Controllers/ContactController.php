fd11d3c71646833c4b5dd7ed92f8d332271065e3
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-02-17 12:26:40 +0100

    verder validatie

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -11,15 +12,10 @@ class ContactController extends Controller
         return view('form');
     }
 
-    public function handle(Request $request)
+    public function handle(ContactRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'department' => 'required',
-            'message' => 'required|string|min:20',
-            'cookie_policy' => 'accepted',
-        ]);
+        $validatedData = $request->validated();
+
         return back()->with('success', 'Thank you for your message. We will get back to you soon.');
     }
 }

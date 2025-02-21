311d70488079bebcf965cd146d10743ebbbea199
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-02-21 12:29:49 +0100

    Add contact form email functionality with validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
+use App\Mail\ContactFormMail;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
@@ -15,6 +17,7 @@ class ContactController extends Controller
     public function handle(ContactRequest $request)
     {
         $validatedData = $request->validated();
+        Mail::to('admin@website.be')->send(new ContactFormMail($validatedData));
 
         return back()->with('success', 'Thank you for your message. We will get back to you soon.');
     }
---
+++ resources/views/emails/contact-form-mail.blade.php
@@ -0,0 +1,5 @@
+<h1>Contact bericht</h1>
+<p>Van: {{ $data['name'] }}</p>
+<p>Email: {{ $data['email'] }}</p>
+<p>Voor: {{ $data['department'] }}</p>
+<p>Bericht: {{ $data['message'] }}</p>
